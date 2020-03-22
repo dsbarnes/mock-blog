ec9b507ffbab91d5a18764d7981f435ad1228d9c
Derek <derekb0147@gmail.com>
2020-03-22 12:55:11 -0400

    Updates

---
+++ precache-manifest.9b25133708bfb6f35bec81ff2090ec79.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac3f8b36dd3422396acc4412e14c320",
+    "url": "/dsbarnes/mock-blog/index.html"
+  },
+  {
+    "revision": "cd46f915fccaf5438ca8",
+    "url": "/dsbarnes/mock-blog/static/css/main.c9525e03.chunk.css"
+  },
+  {
+    "revision": "461b5abb2136abd98541",
+    "url": "/dsbarnes/mock-blog/static/js/2.c5154df8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/dsbarnes/mock-blog/static/js/2.c5154df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd46f915fccaf5438ca8",
+    "url": "/dsbarnes/mock-blog/static/js/main.2bf935dc.chunk.js"
+  },
+  {
+    "revision": "98c0be51600705c1f585",
+    "url": "/dsbarnes/mock-blog/static/js/runtime-main.8fdc3655.js"
+  },
+  {
+    "revision": "c66e2083dac23f07520531b058eccef2",
+    "url": "/dsbarnes/mock-blog/static/media/Bash.c66e2083.svg"
+  },
+  {
+    "revision": "a9c4890cc574e8999347106050a397cc",
+    "url": "/dsbarnes/mock-blog/static/media/CSharp.a9c4890c.svg"
+  },
+  {
+    "revision": "b28354b934074bffb7c78830c85fbc5f",
+    "url": "/dsbarnes/mock-blog/static/media/Env.b28354b9.svg"
+  },
+  {
+    "revision": "90391fdd2d12fbecf975ae999baf055e",
+    "url": "/dsbarnes/mock-blog/static/media/JavaScript.90391fdd.svg"
+  },
+  {
+    "revision": "258e95bdcb6a5e7c5bde1afa1872a7d2",
+    "url": "/dsbarnes/mock-blog/static/media/Kata.258e95bd.svg"
+  },
+  {
+    "revision": "9bd9433e90d9763d24011237e1038858",
+    "url": "/dsbarnes/mock-blog/static/media/Python.9bd9433e.svg"
+  },
+  {
+    "revision": "6aaed72b59b3ae3ff0724f776266f11f",
+    "url": "/dsbarnes/mock-blog/static/media/Rust.6aaed72b.svg"
+  }
+]);
