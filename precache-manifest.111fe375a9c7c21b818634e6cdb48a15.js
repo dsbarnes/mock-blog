4b1bf430c59d45608eda66ead5b60ed29754ce3e
Derek <derekb0147@gmail.com>
2020-03-22 13:52:56 -0400

    Updates

---
+++ precache-manifest.111fe375a9c7c21b818634e6cdb48a15.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f351a7ddab09d910c5066d6512074c",
+    "url": "/dsbarnes/mock-blog/index.html"
+  },
+  {
+    "revision": "5c5622f9593467f1911a",
+    "url": "/dsbarnes/mock-blog/static/css/main.c9525e03.chunk.css"
+  },
+  {
+    "revision": "4ba89c4032d8b3bd377b",
+    "url": "/dsbarnes/mock-blog/static/js/2.6844284f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/dsbarnes/mock-blog/static/js/2.6844284f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5622f9593467f1911a",
+    "url": "/dsbarnes/mock-blog/static/js/main.4d85a74c.chunk.js"
+  },
+  {
+    "revision": "98c0be51600705c1f585",
+    "url": "/dsbarnes/mock-blog/static/js/runtime-main.8fdc3655.js"
+  },
+  {
+    "revision": "c66e2083dac23f07520531b058eccef2",
+    "url": "/dsbarnes/mock-blog/static/media/Bash.c66e2083.svg"
+  },
+  {
+    "revision": "a9c4890cc574e8999347106050a397cc",
+    "url": "/dsbarnes/mock-blog/static/media/CSharp.a9c4890c.svg"
+  },
+  {
+    "revision": "b28354b934074bffb7c78830c85fbc5f",
+    "url": "/dsbarnes/mock-blog/static/media/Env.b28354b9.svg"
+  },
+  {
+    "revision": "90391fdd2d12fbecf975ae999baf055e",
+    "url": "/dsbarnes/mock-blog/static/media/JavaScript.90391fdd.svg"
+  },
+  {
+    "revision": "258e95bdcb6a5e7c5bde1afa1872a7d2",
+    "url": "/dsbarnes/mock-blog/static/media/Kata.258e95bd.svg"
+  },
+  {
+    "revision": "9bd9433e90d9763d24011237e1038858",
+    "url": "/dsbarnes/mock-blog/static/media/Python.9bd9433e.svg"
+  },
+  {
+    "revision": "6aaed72b59b3ae3ff0724f776266f11f",
+    "url": "/dsbarnes/mock-blog/static/media/Rust.6aaed72b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.13e6b5ef70001c3c115b1769b1e18587.js"
+  "/dsbarnes/mock-blog/precache-manifest.111fe375a9c7c21b818634e6cdb48a15.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dsbarnes/mock-blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
